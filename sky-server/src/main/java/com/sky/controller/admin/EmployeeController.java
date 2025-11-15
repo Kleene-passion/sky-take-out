940579e33168785cbc0284905fc7ee28f9aabcbd
Kleene-passion <1774689378@qq.com>
2025-11-15 19:28:54 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -101,4 +101,18 @@ public class EmployeeController {
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+
+    /**
+     * 启用/禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation(value = "启用/禁用员工账号")
+    public Result starOrStop(@PathVariable Integer status, Long id){
+        log.info("启用/禁用员工账号,参数为：{}，{}", status, id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -32,4 +32,9 @@ public interface EmployeeMapper {
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+    /**
+     * 启用/禁用员工账号
+     * @param employee
+     */
+    void update(Employee employee);
 }
