0016908b65656875b515a6e105dbf78408d73d62
Kleene-passion <1774689378@qq.com>
2025-11-24 06:54:55 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,10 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -51,4 +55,13 @@ public interface OrderMapper {
       */
     @Select("select count(*) from sky_take_out.orders where status = #{status}")
     Integer countStatus(Integer status);
+
+     /**
+      * 根据状态和订单时间查询订单
+      * @param status 订单状态
+      * @param orderTime 订单时间
+      * @return 订单列表
+      */
+    @Select("select * from sky_take_out.orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
