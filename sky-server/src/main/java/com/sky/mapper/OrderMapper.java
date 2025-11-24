0ee7072ddcd485f8f0dcc460705c565bda3440ba
Kleene-passion <1774689378@qq.com>
2025-11-24 16:36:01 +0800

    订单数据统计功能代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -72,4 +72,11 @@ public interface OrderMapper {
       * @return 营业额
       */
      Double sumByMap(Map map);
+
+     /**
+      * 根据动态条件统计订单数量
+      * @param map 查询参数
+      * @return 订单数量
+      */
+     Integer countByMap(Map map);
 }
