25decab2b4f67c809ef40dbabb16e95418b59028
Kleene-passion <1774689378@qq.com>
2025-11-25 00:30:53 +0800

    导出运营数据表代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -90,5 +92,16 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("销量排名前10：{} {}", begin, end);
         return Result.success(reportService.getSalesTop10(begin, end));
+    }
+
+    /**
+     * 导出数据统计报表
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出数据统计报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
+
     }
 }
